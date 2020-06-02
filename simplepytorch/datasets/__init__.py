cfd99af65111c571ec7f0fd214a1f74abb8dbd4d
Alex Gaudio <adgaudio@gmail.com>
2020-06-02 16:58:31 -0400

    add some comments to datasets.__init__

--- simplepytorch/datasets/__init__.py
+++ simplepytorch/datasets/__init__.py
@@ -3,10 +3,15 @@ Load datasets
 """
 from .train_val_test_split import train_val_test_split
 from .glob_image_dir import GlobImageDir
+from .pickled_dicts import PickledDicts
+
+# retinal fundus images
 from .messidor import Messidor
 from .eyepacs import Eyepacs
 from .rite import RITE
 from .idrid import IDRiD_Segmentation
+# --> private dataset
 from .qualdr import QualDR, QualDR_Grading
-from .pickled_dicts import PickledDicts
+
+# Chest X-Ray
 from .chexpert import CheXpert, CheXpert_Small
