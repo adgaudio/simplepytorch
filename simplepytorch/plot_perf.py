393068303c51cf8dec42d72413133f4516a2c79b
Alex Gaudio <adgaudio@gmail.com>
2020-11-19 06:30:42 -0500

    plot has a mode 3 that combines modes 1 and 2

--- simplepytorch/plot_perf.py
+++ simplepytorch/plot_perf.py
@@ -14,16 +14,18 @@ import sys
 import re
 import glob
 import os
-from os.path import join, exists, basename
+from os.path import join, exists, basename, dirname
 #  from os import exists
 import pandas as pd
+import seaborn as sns
+from textwrap import dedent
 from matplotlib import pyplot as plt
 #  import mpld3
 #  import mpld3.plugins
 
 
 def get_run_ids(ns):
-    dirs = glob.glob(join(ns.data_dir, 'results/*'))
+    dirs = glob.glob(join(ns.data_dir, '*'))
     for dir in dirs:
         run_id = basename(dir)
         if not re.search(ns.runid_regex, basename(dir)):
@@ -33,10 +35,14 @@ def get_run_ids(ns):
 
 def load_df_from_fp(fp, ns):
     print('found data', fp)
-    if fp.endswith('.csv'):
-        df = pd.read_csv(fp).set_index(ns.index_col)
-    elif fp.endswith('.h5'):
-        df = pd.read_hdf(fp, ns.hdf_table)
+    try:
+        if fp.endswith('.csv'):
+            df = pd.read_csv(fp).set_index(ns.index_col)
+        elif fp.endswith('.h5'):
+            df = pd.read_hdf(fp, ns.hdf_table)
+    except:
+        print(f"   WARNING: failed to load {fp}")
+        df = pd.DataFrame()
     return df
 
 
@@ -44,7 +50,7 @@ def _mode1_get_frames(ns):
     for run_id in get_run_ids(ns):
         #  if not exists(join(dir, 'lock.finished')):
             #  continue
-        dirp = f'{ns.data_dir}/results/{run_id}'
+        dirp = f'{ns.data_dir}/{run_id}'
         gen = (
             ((run_id, fname), load_df_from_fp(join(dirp, fname), ns))
             for fname in os.listdir(dirp)
@@ -56,7 +62,7 @@ def make_plots(ns, cdfs):
     plot_cols = [col for col in cdfs.columns if re.search(ns.col_regex, col)]
     for col in plot_cols:
         fig, ax = plt.subplots(1,1)
-        df = cdfs[col].reset_index('filename', drop=True).unstack('run_id')
+        df = cdfs[col].unstack(level=0)
         if ns.rolling_mean:
             df = df.rolling(ns.rolling_mean).mean()
         _plot_lines = df.plot(ax=ax, title=col)
@@ -68,6 +74,7 @@ def make_plots(ns, cdfs):
 
 
 def savefig_with_symlink(fig, fp, symlink_fp):
+    os.makedirs(dirname(fp), exist_ok=True)
     fig.savefig(fp, bbox_inches='tight')
     if os.path.islink(symlink_fp):
         os.remove(symlink_fp)
@@ -83,6 +90,8 @@ def main(ns):
         mode_1_plots(ns)
     elif ns.mode == 2:
         mode_2_plots(ns)
+    elif ns.mode == 3:
+        mode_3_plots(ns)
     else:
         raise Exception(f'not implemented mode: {ns.mode}')
     if ns.no_plot:
@@ -98,7 +107,10 @@ def _mode_1_get_perf_data_as_df(ns):
 
 
 def mode_1_plots(ns):
-    cdfs = _mode_1_get_perf_data_as_df(ns)
+    """Compare experiments.  
+    One plot for each metric, comparing the most recent result of each experiment
+    """
+    cdfs = _mode_1_get_perf_data_as_df(ns).reset_index('filename', drop=True)
     globals().update({'cdfs_mode1': cdfs})
 
     timestamp = dt.datetime.utcnow().strftime('%Y%m%dT%H%M%S')  # date plot was created.  nothing to do with timestamp column.
@@ -112,11 +124,14 @@ def mode_1_plots(ns):
 
 
 def mode_2_plots(ns):
+    """Analyze an experiment's historical performance across many runs.
+    One plot for each experiment and metric, to analyze history of runs for
+    that experiment"""
     # mode 2: compare train to val performance
     cdfs_mode2 = {}
     timestamp = dt.datetime.utcnow().strftime('%Y%m%dT%H%M%S')  # date plot was created.  nothing to do with timestamp column.
     for run_id in get_run_ids(ns):
-        dirp = f'{ns.data_dir}/results/{run_id}/log'
+        dirp = f'{ns.data_dir}/{run_id}/log'
         if not os.path.exists(dirp):
             print('skip', run_id, 'contains no log data')
             continue
@@ -125,11 +140,13 @@ def mode_2_plots(ns):
             (run_id, fname): load_df_from_fp(join(dirp, fname), ns)
             for fname in os.listdir(dirp)
             if re.search(f'{ns.data_fp_regex}', fname)},
-            sort=False, names=['run_id', 'filename'])
+            sort=False, names=['run_id', 'filename']
+        )
         cdfs_mode2[run_id] = cdfs
 
         os.makedirs(f'{ns.mode2_savefig_dir}/archive'.format(run_id=run_id), exist_ok=True)
-        for fig, ax, col in make_plots(ns, cdfs):
+        for fig, ax, col in make_plots(ns, cdfs.reset_index('run_id', drop=True)):
+            fig.suptitle(run_id)
             if not ns.savefig: continue
             # save to file
             savefig_with_symlink(
@@ -139,21 +156,65 @@ def mode_2_plots(ns):
     globals().update({'cdfs_mode2': cdfs_mode2})
 
 
+def mode_3_plots(ns):
+    """Compare across experiments, considering their history of runs.
+    Basically combines mode 1 and mode 2. """
+    cdfs_mode3 = {}
+    timestamp = dt.datetime.utcnow().strftime('%Y%m%dT%H%M%S')  # date plot was created.  nothing to do with timestamp column.
+    for run_id in get_run_ids(ns):
+        dirp = f'{ns.data_dir}/{run_id}/log'
+        if not os.path.exists(dirp):
+            print('skip', run_id, 'contains no log data')
+            continue
+
+        cdfs = pd.concat({
+            (run_id, fname): load_df_from_fp(join(dirp, fname), ns)
+            for fname in os.listdir(dirp)
+            if re.search(f'{ns.data_fp_regex}', fname)},
+            sort=False, names=['run_id', 'filename']
+        )
+        cdfs_mode3[run_id] = cdfs
+    cdfs = pd.concat(cdfs_mode3.values())
+
+    #  cdfs.groupby(['run_id', 'epoch']).agg(ns.mode3_agg_method)
+    cdfs_unmodified = cdfs.copy()
+    if ns.rolling_mean:
+        cdfs = cdfs.rolling(ns.rolling_mean).mean()
+
+    plot_cols = [col for col in cdfs.columns if re.search(ns.col_regex, col)]
+    for col in plot_cols:
+        fig, ax = plt.subplots(1,1)
+        sns.lineplot(x='epoch', y=col, hue='run_id', ax=ax, data=cdfs)
+        ax.set_title(col)
+        if not ns.savefig: continue
+        savefig_with_symlink(
+            fig,
+            f'{ns.mode3_savefig_dir}/archive/{col}_mode3_{timestamp}.png',
+            f'{ns.mode3_savefig_dir}/{col}_mode3_latest.png')
+    globals().update({'cdfs_mode3': cdfs_unmodified})
+
+
 def bap():
-    par = ap.ArgumentParser(formatter_class=ap.ArgumentDefaultsHelpFormatter)
+    class F(ap.ArgumentDefaultsHelpFormatter, ap.RawTextHelpFormatter): pass
+    par = ap.ArgumentParser(formatter_class=F)
     A = par.add_argument
     A('runid_regex', help='find the run_ids that match the regular expression.')
-    A('--data-dir', default='./data', help=' ')
+    A('--data-dir', default='./results', help=' ')
     A('--data-fp-regex', default='perf.*\.csv', help=' ')
     #  A('--', nargs='?', default='one', const='two')
     A('--rolling-mean', '--rm', type=int, help='smoothing')
     A('--hdf-table-name', help='required if searching .h5 files')
     A('-c', '--col-regex', default='^(?!epoch|batch_idx|timestamp)', help='plot only columns matching regex.  By default, plot all except epoch and batch_idx.')
     A('--index-col', default='epoch', help=' ')
-    A('--mode', default=1, type=int, choices=[1,2],
-      help="`--mode 1` compares each column across all run_ids. `--mode 2` visualize the history of each column of a single run id.")
-    A('--mode1-savefig-dir', default='./data/plots', help=' ')
-    A('--mode2-savefig-dir', default='./data/results/{run_id}/plots', help=' ')
+    A('--mode', default=1, type=int, choices=[1,2,3], help=dedent('''\
+        `--mode 1` compare across experiments, with one plot per column (i.e. performance metric)
+        `--mode 2` Within one experiment and column, visualize the history of all runs.
+        `--mode 3` combine 1 and 2.  compare across run_ids, with
+                   confidence interval to consider history of runs for each run_id.'''))
+    A('--mode1-savefig-dir', default='./results/plots/mode1', help=' ')
+    A('--mode2-savefig-dir', default='./results/plots/mode2', help=' ')
+    A('--mode3-savefig-dir', default='./results/plots/mode3', help=' ')
+    A('--best-effort', action='store_true', help=" Try to load a csv file, but don't raise error if cannot read a file.")
     A('--savefig', action='store_true', help="save plots to file")
     A('--no-plot', action='store_true', help="if supplied, don't show the plots")
     return par
